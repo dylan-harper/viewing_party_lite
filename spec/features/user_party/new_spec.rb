dd657a187e9319c78d86b30680f391bfde26aaa5
Dylan Harper <dylanharper4@icloud.com>
2022-02-04 10:45:23 -0800

    Finish basic setup for last user story

--- spec/features/user_party/new_spec.rb
+++ spec/features/user_party/new_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Viewing Party Create Page' do
   before :each do
     @movie = MovieFacade.all_movies.first
     @user = create(:user)
-
+    @name = @user.name.capitalize
     visit "/users/#{@user.id}/movies/#{@movie.db_id}/viewing-party/new"
   end
 
@@ -32,8 +32,25 @@ RSpec.describe 'Viewing Party Create Page' do
     users = User.all
 
     users.each do |user|
-      name = user.name.first.upcase
-      expect(page).to have_content(name)
+      # name = user.name.first.upcase
+      expect(page).to have_content(@name)
     end
+  end
+
+  it 'creates party and links to user show page', :vcr do
+    fill_in :duration, with: '200'
+
+    date = Time.now.tomorrow
+    date = date.strftime('%Y-%m-%d')
+    fill_in :select_date, with: date
+
+    fill_in :start_time, with: (Time.now + 1*30*45).to_s
+
+    check "#{@user.name}"
+
+    click_button "Create Party"
+
+    expect(current_path).to eq("/users/#{@user.id}")
+    expect(page).to have_content(@movie.title)
   end
 end
